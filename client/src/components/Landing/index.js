55e43ad76ccbf2727f4406d8f952b4c7a8fc73d2
vithyeakim <79346274+vithyeakim@users.noreply.github.com>
2023-02-25 18:21:20 -0500

    adding navigation bar and routing

---
+++ client/src/components/Landing/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import Link from '@material-ui/core/Link';
+import history from '../Navigation/history';
+
+const Landing = () => {
+    return (
+        <div>
+            <Typography variant="h3" color="inherit" noWrap>
+                This is Landing Page
+
+</Typography>
+        </div>
+    )
+}
+export default Landing;
