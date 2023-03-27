d54b25280ae93bb34263dbdd59bcbddc664e9b45
anujgarlapati <a2garlap@uwaterloo.ca>
2023-03-26 20:33:26 -0400

    adding images to the guess the team game

---
+++ client/src/components/GuessTheTeamQuestions/NBATeamQuestions.js
@@ -0,0 +1,129 @@
+const questions = [
+    {
+      answer: "Atlanta Hawks",
+      question: "https://i.ibb.co/wJJyH5f/Screenshot-2023-03-26-at-6-08-21-PM-Small.jpg"
+    },
+    {
+      answer: "Boston Celtics",
+      question: "https://upload.wikimedia.org/wikipedia/en/8/8f/Boston_Celtics.svg"
+    },
+    {
+      answer: "Brooklyn Nets",
+      question: "https://upload.wikimedia.org/wikipedia/commons/4/44/Brooklyn_Nets_newlogo.svg"
+    },
+    {
+      answer: "Charlotte Hornets",
+      question: "https://upload.wikimedia.org/wikipedia/en/c/c4/Charlotte_Hornets_%282014%29.svg"
+    },
+    {
+      answer: "Chicago Bulls",
+      question: "https://upload.wikimedia.org/wikipedia/en/6/67/Chicago_Bulls_logo.svg"
+    },
+    {
+      answer: "Cleveland Cavaliers",
+      question: "https://upload.wikimedia.org/wikipedia/en/4/4b/Cleveland_Cavaliers_logo.svg"
+    },
+    {
+      answer: "Dallas Mavericks",
+      question: "https://upload.wikimedia.org/wikipedia/en/9/97/Dallas_Mavericks_logo.svg"
+    },
+    {
+      answer: "Denver Nuggets",
+      question: "https://upload.wikimedia.org/wikipedia/en/7/76/Denver_Nuggets.svg"
+    },
+    {
+      answer: "Detroit Pistons",
+      question: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Detroit_Pistons_logo.svg"
+    },
+    {
+      answer: "Golden State Warriors",
+      question: "https://upload.wikimedia.org/wikipedia/en/0/01/Golden_State_Warriors_logo.svg"
+    },
+    {
+      answer: "Houston Rockets",
+      question: "https://upload.wikimedia.org/wikipedia/en/2/28/Houston_Rockets.svg"
+    },
+    {
+      answer: "Indiana Pacers",
+      question: "https://upload.wikimedia.org/wikipedia/en/1/1b/Indiana_Pacers.svg"
+    },
+    {
+      answer: "Los Angeles Clippers",
+      question: "https://upload.wikimedia.org/wikipedia/en/b/bb/Los_Angeles_Clippers_%282015%29.svg"
+    },
+    {
+      answer: "Los Angeles Lakers",
+      question: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Los_Angeles_Lakers_logo.svg"
+    },
+    {
+      answer: "Memphis Grizzlies",
+      question: "https://upload.wikimedia.org/wikipedia/en/f/f1/Memphis_Grizzlies.svg"
+    },
+    {
+      answer: "Miami Heat",
+      question: "https://upload.wikimedia.org/wikipedia/en/f/fb/Miami_Heat_logo.svg"
+    },
+    {
+      answer: "Milwaukee Bucks",
+      question: "https://upload.wikimedia.org/wikipedia/en/4/4a/Milwaukee_Bucks_logo.svg"
+    },
+    {
+      answer: "Minnesota Timberwolves",
+      question: "https://upload.wikimedia.org/wikipedia/en/c/c2/Minnesota_Timberwolves_logo.svg"
+    },
+    {
+      answer: "New Orleans Pelicans",
+      question: "https://upload.wikimedia.org/wikipedia/en/0/0d/New_Orleans_Pelicans_logo.svg"
+    },
+    {
+        answer: "New York Knicks",
+        question: "https://upload.wikimedia.org/wikipedia/en/2/25/New_York_Knicks_logo.svg"
+    },
+    {
+        answer: "Oklahoma City Thunder",
+        question: "https://upload.wikimedia.org/wikipedia/en/5/5d/Oklahoma_City_Thunder.svg"
+    },
+    {
+        answer: "Orlando Magic",
+        question: "https://upload.wikimedia.org/wikipedia/en/1/10/Orlando_Magic_logo.svg"
+    },
+    {
+        answer: "Philadelphia 76ers",
+        question: "https://upload.wikimedia.org/wikipedia/en/0/0e/Philadelphia_76ers_logo.svg"
+    },
+    {
+        answer: "Phoenix Suns",
+        question: "https://upload.wikimedia.org/wikipedia/en/d/dc/Phoenix_Suns_logo.svg"
+    },
+    {
+        answer: "Portland Trail Blazers",
+        question: "https://upload.wikimedia.org/wikipedia/en/2/21/Portland_Trail_Blazers_logo.svg"
+    },
+    {
+        answer: "Sacramento Kings",
+        question: "https://upload.wikimedia.org/wikipedia/en/c/c7/SacramentoKings.svg"
+    },
+    {
+        answer: "San Antonio Spurs",
+        question: "https://upload.wikimedia.org/wikipedia/en/a/a2/San_Antonio_Spurs.svg"
+    },
+    {
+        answer: "Toronto Raptors",
+        question: "https://upload.wikimedia.org/wikipedia/en/3/36/Toronto_Raptors_logo.svg"
+    },
+    {
+        answer: "Utah Jazz",
+        question: "https://upload.wikimedia.org/wikipedia/en/0/04/Utah_Jazz_logo_%282016%29.png"
+    },
+    {
+        answer: "Washington Wizards",
+        question: "https://upload.wikimedia.org/wikipedia/en/0/02/Washington_Wizards_logo.svg"
+    }
+    ];
+
+    const NBATeamQuestions = questions.sort()
+
+    export default NBATeamQuestions;
+
+
+//    "https://i.ibb.co/wJJyH5f/Screenshot-2023-03-26-at-6-08-21-PM-Small.jpg"
