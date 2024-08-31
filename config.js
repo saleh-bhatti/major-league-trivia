51923f2b5aab2b14707bcdc0814436965def6b90
saleh-bhatti <90345337+saleh-bhatti@users.noreply.github.com>
2024-08-31 15:51:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
   host    : 'http://ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : 's72bhatt',
-  password: 'Nshpmc33!',
+  user    : 's75bhatt',
+  password: '12345678!',
   database: 'a3kler'
 };
  
